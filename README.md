e05d093901d26d852e35999f0d096014412c22ca
ApocalypseCalculator <>
2021-02-08 18:51:42 -0500

    blocc

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AnotherAdBlock
+
 Yet another Ad Block, made for efficiency and simplicity
+
+It simply blocks all connections to the blacklisted domains
+
+
+
+TO-DO: 
+
+Add some graphical interface
---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.runtime.getPackageDirectoryEntry(function (root) {
+    root.getFile("./data/list.json", {}, function (fileEntry) {
+        fileEntry.file(function (file) {
+            var reader = new FileReader();
+            reader.onloadend = function (e) {
+                let parsed = JSON.parse(this.result);
+                chrome.webRequest.onBeforeRequest.addListener(
+                    function (details) { return { cancel: true }; },
+                    { urls: parsed },
+                    ["blocking"]
+                );
+            };
+            reader.readAsText(file);
+        });
+    });
+})
