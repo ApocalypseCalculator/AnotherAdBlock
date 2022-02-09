4e67b13f7bb99b732d02fdf8009e0e9a9c8c7c98
Patrick Lin <apocalypsecalculator@gmail.com>
2022-02-08 19:10:53 -0500

    remove bad headers

--- background.js
+++ background.js
@@ -6,12 +6,32 @@ chrome.runtime.getPackageDirectoryEntry(function (root) {
                 let parsed = JSON.parse(this.result);
                 chrome.webRequest.onBeforeRequest.addListener(
                     function (details) { return { cancel: true }; },
-                    { urls: parsed },
+                    { urls: parsed.blockedSites },
                     ["blocking"]
                 );
+                chrome.webRequest.onBeforeSendHeaders.addListener(
+                    function (details) {
+                        for (var i = details.requestHeaders.length - 1; i >= 0; i--) {
+                            if (parsed.blockedHeaders.includes(`${details.requestHeaders[i].name}`.toLowerCase())) {
+                              details.requestHeaders.splice(i, 1);
+                            }
+                        }
+                        return { requestHeaders: details.requestHeaders };
+                    },
+                    {urls: ['<all_urls>']},
+                    ['blocking', 'requestHeaders']
+                );
+                /*chrome.webRequest.onHeadersReceived.addListener(
+                    function (details) {
+                        // check Set Cookie headers for tracker cookies
+                        return { responseHeaders: details.responseHeaders };
+                    },
+                    {urls: ['<all_urls>']},
+                    ['blocking', 'responseHeaders']
+                )*/
                 chrome.browserAction.setBadgeText({ text: 'ON' });
                 chrome.browserAction.setBadgeBackgroundColor({ color: '#4688F1' });
-                chrome.browserAction.setTitle({ title: "Another Ad Block is Active" });
+                chrome.browserAction.setTitle({ title: "Another Ad Block is Active. Press Ctrl+Shift+A to toggle on/off" });
             };
             reader.readAsText(file);
         });
