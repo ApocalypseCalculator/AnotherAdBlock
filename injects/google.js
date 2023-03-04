abca286c853bba80c8bb4ef73f40fbe885bce8d7
Patrick Lin <apocalypsecalculator@gmail.com>
2023-03-04 00:00:13 -0500

    remove sponsored results from google

---
+++ injects/google.js
@@ -0,0 +1,2 @@
+document.getElementById("tvcap").innerHTML = ""; //remove sponsored section
+document.getElementById("res").className = document.getElementById("res").className.split(/\s+/g)[0]; //eqAnXb, the class name for main search results without ads
