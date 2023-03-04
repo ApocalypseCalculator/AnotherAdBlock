1ca6ae6ec13995aea8de40a3fd000bf563b8ac86
Patrick Lin <apocalypsecalculator@gmail.com>
2023-03-04 00:00:00 -0500

    dedicated yt script

---
+++ injects/youtube.js
@@ -0,0 +1,17 @@
+try {
+    const clear = (() => {
+        const defined = v => v !== null && v !== undefined;
+        const timeout = setInterval(() => {
+            const ad = [...document.querySelectorAll('.ad-showing')][0];
+            if (defined(ad)) {
+                const video = document.querySelector('video');
+                if (defined(video)) {
+                    video.currentTime = video.duration;
+                }
+            }
+        }, 500);
+        return function () {
+            clearTimeout(timeout);
+        }
+    })();
+} catch {}
