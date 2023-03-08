2fed4aaa6a00fb9fab6bec9dde4865f7a4477719
ApocalypseCalculator <59318806+ApocalypseCalculator@users.noreply.github.com>
2023-03-08 00:12:23 -0500

    remove sponsored shopping on right hand bar

--- injects/google.js
+++ injects/google.js
@@ -1,2 +1,6 @@
 document.getElementById("tvcap").innerHTML = ""; //remove sponsored section
 document.getElementById("res").className = document.getElementById("res").className.split(/\s+/g)[0]; //eqAnXb, the class name for main search results without ads
+let sponsored = document.getElementsByClassName("cu-container");
+for(let i = 0; i < sponsored.length; i++){
+	sponsored.item(i).innerHTML = "";
+}
