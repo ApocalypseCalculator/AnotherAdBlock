ea5d8537f711fcd7865913d758aadd6e171d6849
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2023-12-12 18:09:10 -0500

    catch error in google inject

--- injects/google.js
+++ injects/google.js
@@ -1,6 +1,8 @@
-document.getElementById("tvcap").innerHTML = ""; //remove sponsored section
-document.getElementById("res").className = document.getElementById("res").className.split(/\s+/g)[0]; //eqAnXb, the class name for main search results without ads
-let sponsored = document.getElementsByClassName("cu-container");
-for(let i = 0; i < sponsored.length; i++){
-	sponsored.item(i).innerHTML = "";
-}
+try {
+	document.getElementById("tvcap").innerHTML = ""; //remove sponsored section
+	document.getElementById("res").className = document.getElementById("res").className.split(/\s+/g)[0]; //eqAnXb, the class name for main search results without ads
+	let sponsored = document.getElementsByClassName("cu-container");
+	for (let i = 0; i < sponsored.length; i++) {
+		sponsored.item(i).innerHTML = "";
+	}
+} catch { }
