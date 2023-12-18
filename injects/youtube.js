447088376c85922632352330cbc724c14962f1d5
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2023-12-18 18:49:58 -0500

    fix youtube ad skip

--- injects/youtube.js
+++ injects/youtube.js
@@ -4,23 +4,26 @@ try {
         const timeout = setInterval(() => {
             const ad = [...document.querySelectorAll('.ad-showing')][0];
             if (defined(ad)) {
-                const video = document.querySelector('video');
-                if (defined(video)) {
-                    video.currentTime = video.duration;
+                const video = document.querySelectorAll('video');
+                try {
+                    if (video.length > 0) {
+                        video[video.length - 1].currentTime = video[video.length - 1].duration;
+                    }
                 }
+                catch { }
             }
         }, 500);
         return function () {
             clearTimeout(timeout);
         }
     })();
-    
+
     const interval = setInterval(() => {
         let ads = document.querySelectorAll('ytd-search-pyv-renderer,ytd-ad-slot-renderer,div[id="player-ads"]');
-        if(ads.length > 0) {
+        if (ads.length > 0) {
             ads.forEach(e => {
                 e.remove();
             });
         }
     }, 200);
-} catch {}
+} catch { }
