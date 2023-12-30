db9ebc7bbf2290e2e84e5c184486a5db8589f227
Patrick Lin <apocalypsecalculator@gmail.com>
2023-12-30 01:18:14 -0500

    add amazon inject

---
+++ injects/amazon.js
@@ -0,0 +1,15 @@
+console.log('[AnotherAdBlock]: Amazon script injected!');
+
+setInterval(() => {
+    try {
+        let adlist = document.querySelectorAll(`div.AdHolder,div:has(>div[data-ad-details])`);
+        if(adlist.length > 0) {
+            adlist.forEach(el => {
+                // deleting the sponsored ad
+                el.remove();
+            });
+            console.log(`[AnotherAdBlock]: Removed ${adlist.length} sponsored listings from search view`);
+        }
+    }
+    catch { }
+}, 100);
