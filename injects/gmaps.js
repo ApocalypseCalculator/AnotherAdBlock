b9b473f81c59b7306135bb1f882b73db4ed1c193
Patrick Lin <apocalypsecalculator@gmail.com>
2023-12-30 01:19:39 -0500

    less clunky gmaps script

--- injects/gmaps.js
+++ injects/gmaps.js
@@ -2,11 +2,11 @@ console.log('[AnotherAdBlock]: Google Maps script injected!');
 
 setInterval(() => {
     try {
-        let adlist = document.querySelectorAll(`div>div>div>div>div>h1[aria-label="Sponsored"]`)
+        let adlist = document.querySelectorAll(`div:has(>div>div>div>div>h1[aria-label="Sponsored"])`)
         if(adlist.length > 0) {
             adlist.forEach(el => {
                 // deleting the sponsored ad
-                el.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+                el.remove();
             });
             console.log(`[AnotherAdBlock]: Removed ${adlist.length} sponsored listings from search view`);
         }
