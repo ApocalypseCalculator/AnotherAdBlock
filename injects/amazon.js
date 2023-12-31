d06ff452a17f62260f1955a2d86e098c45d6e8e3
Patrick Lin <apocalypsecalculator@gmail.com>
2023-12-31 02:03:39 -0500

    update amazon script

--- injects/amazon.js
+++ injects/amazon.js
@@ -2,7 +2,7 @@ console.log('[AnotherAdBlock]: Amazon script injected!');
 
 setInterval(() => {
     try {
-        let adlist = document.querySelectorAll(`div.AdHolder,div:has(>div[data-ad-details])`);
+        let adlist = document.querySelectorAll(`div.AdHolder,div:has(>div[data-ad-details]),div:has(>div>div>div>div[aria-label="Sponsored Ad"])`);
         if(adlist.length > 0) {
             adlist.forEach(el => {
                 // deleting the sponsored ad
