007ad33d0f08441b2ec794cfdb3fc42a2e804215
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2025-11-13 22:39:39 -0500

    update anti admiral blocker

--- injects/anti_admiral.js
+++ injects/anti_admiral.js
@@ -1,21 +1,51 @@
-let banned_scripts = ['pmc-admiral', 'admiral'];
+const BANNED_SCRIPT_CLASS = ['pmc-admiral', 'admiral'];
+const BANNED_SCRIPT_IDS = ['admiral-script']
+
+window.admiral = function() {
+    console.log('[AnotherAdBlock]: hijacked admiral call');
+}
+
+function removeAdmiralNode(node) {
+    if (
+        node.tagName === "SCRIPT" &&
+        (
+            BANNED_SCRIPT_CLASS.some(banned_script =>
+                node.classList?.contains(banned_script)
+            ) || BANNED_SCRIPT_IDS.some(banned_id =>
+                node.id === banned_id
+            ) || possibleAdmiralScript(node.text)
+        )
+    ) {
+        node.remove(); // Prevent execution
+        console.log(`[AnotherAdBlock]: Blocked admiral script from execution`);
+    }
+}
 
 new MutationObserver((mutations) => {
     for (const mutation of mutations) {
         for (const node of mutation.addedNodes) {
-            if (
-                node.tagName === "SCRIPT" &&
-                banned_scripts.some(banned_script =>
-                    node.classList?.contains(banned_script)
-                )
-            ) {
-                node.remove(); // Prevent execution
-                console.log(`[AnotherAdBlock]: Blocked admiral script from execution`);
-            }
+            removeAdmiralNode(node);
         }
+        removeAdmiralNode(mutation.target);
     }
-}).observe(document.documentElement, { childList: true, subtree: true });
+}).observe(document.documentElement, { childList: true, subtree: true, characterData: true, attributes: true });
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(banned_scripts.map(script => `script.${script}`).join(", ")).forEach(s => s.remove());
+    document.querySelectorAll(
+        BANNED_SCRIPT_CLASS.map(script => `script.${script}`)
+            .concat(BANNED_SCRIPT_IDS.map(id => `script#${id}`))
+            .join(", ")
+    ).forEach(s => s.remove());
 });
+
+function possibleAdmiralScript(content) {
+    if (!content || content == '' || typeof content !== 'string') return false;
+    content = content.toLowerCase();
+    return (
+        content.includes('disable your adblocker') ||
+        content.includes('disabling your adblocker') ||
+        content.includes('disable my adblocker') ||
+        content.includes('continue without supporting us') ||
+        content.includes('window.admiral(')
+    );
+}
